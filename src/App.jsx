82fd6708cb0479adafe80bb0c8aaac4c32992511
suzandev <dev.suzanchandra@gmail.com>
2023-09-17 23:42:04 +0600

    price options project start and tailwindCss setup with vite and react jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
